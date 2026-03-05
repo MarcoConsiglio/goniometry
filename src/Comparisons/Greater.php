77b7f2b5e4e9a7fe98996db65704b10432ce00cd
Marco Consiglio <mrccnsgl@gmail.com>
2026-03-05 15:50:16 +0000

    feat: Greater comparison

---
+++ src/Comparisons/Greater.php
@@ -0,0 +1,22 @@
+<?php
+namespace MarcoConsiglio\Goniometry\Comparisons;
+
+class Greater extends Comparison
+{
+    /**
+     * Set the comparison strategy based on the comparison type and
+     * the type of the right operand of the comparison.
+     */
+    protected function setComparisonStrategy(): void
+    {
+        $this->comparison_strategy = $this->getBetaType()->getStrategyFor($this, $this->alfa);
+    }
+
+    /**
+     * Perform the comparison.
+     */
+    public function compare(): bool
+    {
+        return $this->comparison_strategy->compare();
+    }
+}
