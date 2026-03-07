f53e34eea66d05f23a77488c8d2d30474bfc2ad8
Marco Consiglio <mrccnsgl@gmail.com>
2026-03-07 13:20:23 +0000

    feat: `LesserOrEqualInt` strategy to compare an `Angle` and an `int`

---
+++ tests/Unit/Comparisons/Strategies/LesserOrEqualIntTest.php
@@ -0,0 +1,68 @@
+<?php
+namespace MarcoConsiglio\Goniometry\Tests\Unit\Comparisons\Strategies;
+
+use MarcoConsiglio\Goniometry\Angle;
+use MarcoConsiglio\Goniometry\Builders\FromDecimal;
+use MarcoConsiglio\Goniometry\Builders\FromDegrees;
+use MarcoConsiglio\Goniometry\Comparisons\Strategies\EqualAngle;
+use MarcoConsiglio\Goniometry\Comparisons\Strategies\GreaterAngle;
+use MarcoConsiglio\Goniometry\Comparisons\Strategies\LesserAngle;
+use MarcoConsiglio\Goniometry\Comparisons\Strategies\LesserOrEqualInt;
+use MarcoConsiglio\Goniometry\Degrees;
+use MarcoConsiglio\Goniometry\Minutes;
+use MarcoConsiglio\Goniometry\Seconds;
+use MarcoConsiglio\Goniometry\Tests\TestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DependsOnClass;
+use PHPUnit\Framework\Attributes\TestDox;
+use PHPUnit\Framework\Attributes\UsesClass;
+
+#[TestDox("The LesserInt comparison strategy")]
+#[CoversClass(LesserOrEqualInt::class)]
+#[UsesClass(Angle::class)]
+#[UsesClass(FromDecimal::class)]
+#[UsesClass(FromDegrees::class)]
+#[UsesClass(GreaterAngle::class)]
+#[UsesClass(EqualAngle::class)]
+#[UsesClass(LesserAngle::class)]
+#[UsesClass(Degrees::class)]
+#[UsesClass(Minutes::class)]
+#[UsesClass(Seconds::class)]
+class LesserOrEqualIntTest extends TestCase
+{
+    #[DependsOnClass(EqualAngleTest::class)]
+    #[DependsOnClass(LesserAngleTest::class)]
+    #[TestDox("can compare an Angle instance and a sexagesimal degrees angle measure.")]
+    public function test_compare(): void
+    {
+        /**
+         * Lesser
+         */
+        // Arrange
+        $alfa = $this->randomAngle(max: 180 - self::SSN);
+        $beta = $this->randomDegrees(min: 180);
+
+        // Act & Assert
+        $this->assertTrue(new LesserOrEqualInt($alfa, $beta)->compare());
+
+        /**
+         * Equal
+         */
+        // Arrange
+        $beta = $this->randomDegrees();
+        $alfa = Angle::createFromValues($beta);
+
+        // Act & Assert
+        $this->assertTrue(new LesserOrEqualInt($alfa, $beta)->compare());
+
+        /**
+         * Greater
+         */
+        // Arrange
+        $alfa = $this->randomAngle(min: 180);
+        $beta = $this->randomDegrees(max: 179);
+
+        // Act & Assert
+        $this->assertFalse(new LesserOrEqualInt($alfa, $beta)->compare());
+    }
+}
