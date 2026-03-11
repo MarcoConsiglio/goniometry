38324d0fe7bdd8b2b000616cbae6eb7a26808eba
Marco Consiglio <mrccnsgl@gmail.com>
2026-03-11 20:10:46 +0000

    test: `SexadecimalDegrees` class

---
+++ tests/Feature/SexadecimalDegreesTest.php
@@ -0,0 +1,56 @@
+<?php
+namespace MarcoConsiglio\Goniometry\Tests\Feature;
+
+use MarcoConsiglio\BCMathExtended\Number;
+use MarcoConsiglio\Goniometry\SexadecimalDegrees;
+use MarcoConsiglio\Goniometry\Tests\TestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\TestDox;
+
+#[TestDox("The SexadecimalDegrees class")]
+#[CoversClass(SexadecimalDegrees::class)]
+class SexadecimalDegreesTest extends TestCase
+{
+    #[TestDox("can store a positive sexadecimal value.")]
+    public function test_positive_sexadecimal(): void
+    {
+        // Arrange
+        $value = $this->positiveRandomSexadecimal();
+
+        // Act
+        $sexadecimal = new SexadecimalDegrees($value);
+
+        // Assert
+        $this->assertEquals(
+            $value, $actual = $sexadecimal->value->toFloat(),
+            "$value ≠ $actual"    
+        );
+    }
+
+    #[TestDox("can store a negative sexadecimal value.")]
+    public function test_negative_sexadecimal(): void
+    {
+         // Arrange
+        $value = $this->negativeRandomSexadecimal();
+
+        // Act
+        $sexadecimal = new SexadecimalDegrees($value);
+
+        // Assert
+        $this->assertEquals(
+            $value, $actual = $sexadecimal->value->toFloat(),
+            "$value ≠ $actual"    
+        );       
+    }
+
+    public function test_cast_to_string(): void
+    {
+        // Arrange
+        $value = $this->randomSexadecimal();
+        $sexadecimal = new SexadecimalDegrees($value);
+        $expected_value = new Number($value);
+
+        // Act
+        $this->assertSame("{$expected_value}°", "$sexadecimal");       
+    }
+}
