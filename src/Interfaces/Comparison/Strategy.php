68ad816cb6085aa4cfd1e8315a811230cc4c4dea
Marco Consiglio <mrccnsgl@gmail.com>
2026-03-16 10:11:28 +0000

    doc: `Strategy` interface

--- src/Interfaces/Comparison/Strategy.php
+++ src/Interfaces/Comparison/Strategy.php
@@ -3,7 +3,13 @@ namespace MarcoConsiglio\Goniometry\Comparisons;
 
 use MarcoConsiglio\Goniometry\Angle;
 
+/**
+ * The behavior of a comparing strategy between angles.
+ */
 interface Strategy
 {
+    /**
+     * Perform the comparison.
+     */
     public function compare(): bool;
 }
