afc209cc957b48df57556e8aea281dac127bf354
Marco Consiglio <mrccnsgl@gmail.com>
2026-04-02 13:04:21 +0000

    refactor: `MinutesTest` class

--- tests/Unit/Random/Validator/MinutesTest.php
+++ tests/Unit/Random/Validator/MinutesTest.php
@@ -9,7 +9,7 @@ use PHPUnit\Framework\Attributes\TestDox;
 
 #[TestCase("The Minutes validator")]
 #[CoversClass(MinutesValidator::class)]
-class MinutesTest extends TestCase
+class MinutesTest extends IntegerValidatorTestCase
 {
     #[TestDox("validates a MinutesRange.")]
     public function test_validate(): void
@@ -19,64 +19,54 @@ class MinutesTest extends TestCase
          * $max < 0
          */
         // Arrange
-        $min = -1;
-        $max = -1;
-        $validator = new MinutesValidator;
-
-        // Act
-        $validator->validate($min, $max);
-
-        // Assert
-        $this->assertSame(0, $min);
-        $this->assertSame($this->maxMinutes(), $max);
+        $this->setValidator();
+        $this->setRange(-1, -1);
+        
+        // Act & Assert
+        $this->testValidator($this->allowedMin(), $this->allowedMax());
 
         /**
          * $min ≥ 60
          * $max ≥ 60
          */
         // Arrange
-        $min = Minutes::MAX;
-        $max = Minutes::MAX;
+        $this->setRange(Minutes::MAX, Minutes::MAX);
 
-        // Act
-        $validator->validate($min, $max);
-
-        // Assert
-        $this->assertSame(0, $min);
-        $this->assertSame($this->maxMinutes(), $max);
+        // Act & Assert
+        $this->testValidator($this->allowedMin(), $this->allowedMax());
 
         /**
          * $min < 0
          * $max ≥ 60
          */
         // Arrange
-        $min = -1;
-        $max = Minutes::MAX;
+        $this->setRange(-1, Minutes::MAX);
 
-        // Act
-        $validator->validate($min, $max);
-
-        // Assert
-        $this->assertSame(0, $min);
-        $this->assertSame($this->maxMinutes(), $max);
+        // Act & Assert
+        $this->testValidator($this->allowedMin(), $this->allowedMax());
 
         /**
          * $min ≥ 60
          * $max < 0
          */
         // Arrange
-        $min = Minutes::MAX;
-        $max = -1;
+        $this->setRange(Minutes::MAX, -1);
 
-        // Act
-        $validator->validate($min, $max);
-
-        // Assert
-        $this->assertSame(0, $min);
-        $this->assertSame($this->maxMinutes(), $max);
+        // Act & Assert
+        $this->testValidator($this->allowedMin(), $this->allowedMax());
     }
 
-    protected function maxMinutes(): int
+    protected function setValidator(): void
+    {
+        $this->validator = new MinutesValidator;
+    }
+
+    protected function allowedMin(): int
+    {
+        return 0;
+    }
+
+    protected function allowedMax(): int
     {
         return Minutes::MAX - 1;
     }
