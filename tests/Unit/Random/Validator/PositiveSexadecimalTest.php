b2721357d0235561ac63a0ab620242bba0b16644
Marco Consiglio <mrccnsgl@gmail.com>
2026-04-02 13:40:28 +0000

    refactor: `PositiveSexadecimalTest` class

--- tests/Unit/Random/Validator/PositiveSexadecimalTest.php
+++ tests/Unit/Random/Validator/PositiveSexadecimalTest.php
@@ -4,11 +4,12 @@ namespace MarcoConsiglio\Goniometry\Tests\Unit\Random\Validator;
 use MarcoConsiglio\FakerPhpNumberHelpers\NextFloat;
 use MarcoConsiglio\Goniometry\Degrees;
 use MarcoConsiglio\Goniometry\Random\Validator\PositiveSexadecimal;
+use MarcoConsiglio\Goniometry\SexadecimalDegrees;
 use MarcoConsiglio\Goniometry\Tests\TestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
 
 #[CoversClass(PositiveSexadecimal::class)]
-class PositiveSexadecimalTest extends TestCase
+class PositiveSexadecimalTest extends FloatValidatorTestCase
 {
     public function test_validate(): void
     {
@@ -17,136 +18,105 @@ class PositiveSexadecimalTest extends TestCase
          * $max = INF
          */
         // Arrange
-        $min = INF;
-        $max = INF;
-        $max_degrees = NextFloat::before(Degrees::MAX);
-        $validator = new PositiveSexadecimal;
+        $this->setValidator();
+        $this->setRange(INF, INF);
 
-        // Act
-        $validator->validate($min, $max);
-
-        // Assert
-        $this->assertSame(0.0, $min);
-        $this->assertSame($max_degrees, $max);
+        // Act & Assert
+        $this->testValidator($this->allowedMin(), $this->allowedMax());
 
         /**
          * $min = INF
          * 0 ≤ $max < 360
          */
         // Arramge
-        $min = INF;
-        $max = 7;
+        $this->setRange(INF, 7);
 
-        // Act
-        $validator->validate($min, $max);
-
-        // Assert
-        $this->assertSame(0.0, $min);
-        $this->assertSame(7.0, $max);
+        // Act & Assert
+        $this->testValidator($this->allowedMin(), 7);
 
         /**
          * 0 ≤ $min < 360
          * $max = INF
          */
         // Arrange
-        $min = 30;
-        $max = INF;
+        $this->setRange(30, INF);
 
-        // Act
-        $validator->validate($min, $max);
-
-        // Assert
-        $this->assertSame(30.0, $min);
-        $this->assertSame($max_degrees, $max);
+        // Act & Assert
+        $this->testValidator(30, $this->allowedMax());
 
         /**
          * $min < 0
          * $max < 0
          */
         // Arrange
-        $min = -1;
-        $max = -1;
+        $this->setRange(-1, -1);
 
-        // Act
-        $validator->validate($min, $max);
-
-        // Assert
-        $this->assertSame(0.0, $min);
-        $this->assertSame($max_degrees, $max);
+        // Act & Assert
+        $this->testValidator($this->allowedMin(), $this->allowedMax());
 
         /**
          * 0 ≤ $min < 360
          * $max < 0
          */
         // Arrange
-        $min = 45;
-        $max = -1;
+        $this->setRange(45, -1);
 
-        // Act
-        $validator->validate($min, $max);
-
-        // Assert
-        $this->assertSame($min, 45.0);
-        $this->assertSame($max_degrees, $max);
+        // Act & Assert
+        $this->testValidator(45, $this->allowedMax());
 
         /**
          * $min < 0
          * 0 ≤ $max < 360
          */
         // Arrange
-        $min = -1;
-        $max = 90;
+        $this->setRange(-1, 90);
 
-        // Act
-        $validator->validate($min, $max);
-
-        // Assert
-        $this->assertSame(0.0, $min);
-        $this->assertSame(90.0, $max);
+        // Act & Assert
+        $this->testValidator($this->allowedMin(), 90);
 
         /**
          * $min ≥ 360
          * 0 ≤ $max < 360
          */
         // Arrange
-        $min = 361;
-        $max = 90;
+        $this->setRange(361, 90);
 
-        // Act
-        $validator->validate($min, $max);
-
-        // Assert
-        $this->assertSame(0.0, $min);
-        $this->assertSame(90.0, $max);
+        // Act & Assert
+        $this->testValidator($this->allowedMin(), 90);
 
         /**
          * 0 ≤ $min < 360
          * $max ≥ 360
          */
         // Arrange
-        $min = 90;
-        $max = 361;
+        $this->setRange(90, 361);
 
-        // Act
-        $validator->validate($min, $max);
-
-        // Assert
-        $this->assertSame(90.0, $min);
-        $this->assertSame($max_degrees, $max);
+        // Act & Assert
+        $this->testValidator(90, $this->allowedMax());
 
         /**
          * $min ≥ 360
          * $max ≥ 360
          */
         // Arrange
-        $min = 361;
-        $max = 361;
+        $this->setRange(361, 361);
 
-        // Act
-        $validator->validate($min, $max);
+        // Act & Assert
+        $this->testValidator($this->allowedMin(), $this->allowedMax());
+    }
 
-        // Assert
-        $this->assertSame(0.0, $min);
-        $this->assertSame($max_degrees, $max);
+    protected function setValidator(): void
+    {
+        $this->validator = new PositiveSexadecimal;
+    }
+
+    protected function allowedMin(): float
+    {
+        return 0.0;
+    }
+
+    protected function allowedMax(): float
+    {
+        return NextFloat::before(Degrees::MAX);
     }
 }
