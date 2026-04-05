fef41ad8b5e7795d419ffb749c45b77c43a8b08b
Marco Consiglio <mrccnsgl@gmail.com>
2026-04-05 12:47:40 +0000

    feat: `NegativeRadian` random generator class

---
+++ src/Random/Generator/NegativeRadian.php
@@ -0,0 +1,19 @@
+<?php
+namespace MarcoConsiglio\Goniometry\Random\Generator;
+
+use MarcoConsiglio\Goniometry\Radian;
+use MarcoConsiglio\Goniometry\Random\Generator\Radian as RadianGenerator;
+
+class NegativeRadian extends RadianGenerator
+{
+    public function generate(int $precision = PHP_FLOAT_DIG): Radian
+    {
+        $this->validate();
+        $radian = $this->generator->randomFloat(
+            $this->normalizePrecision($precision),
+            abs($this->range->end),
+            abs($this->range->start)
+        );
+        return new Radian($radian);
+    }
+}
