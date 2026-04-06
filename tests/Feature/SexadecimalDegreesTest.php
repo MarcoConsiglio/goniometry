ecdbb3c02e4b2c3dba59f0b87001f7b4a12dc368
Marco Consiglio <mrccnsgl@gmail.com>
2026-04-06 16:22:19 +0000

    refactor: `SexadecimalDegreesTest` class

--- tests/Feature/SexadecimalDegreesTest.php
+++ tests/Feature/SexadecimalDegreesTest.php
@@ -1,16 +1,31 @@
 <?php
 namespace MarcoConsiglio\Goniometry\Tests\Feature;
 
+use MarcoConsiglio\Goniometry\Random\Generator\NegativeSexadecimal as NegativeSexadecimalGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\PositiveSexadecimal as PositiveSexadecimalGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\RelativeSexadecimal as RelativeSexadecimalGenerator;
+use MarcoConsiglio\Goniometry\Random\Validator\NegativeSexadecimal as NegativeSexadecimalValidator;
+use MarcoConsiglio\Goniometry\Random\Validator\PositiveSexadecimal as PositiveSexadecimalValidator;
+use MarcoConsiglio\Goniometry\Random\Validator\RelativeSexadecimal as RelativeSexadecimalValidator;
+use MarcoConsiglio\Goniometry\Random\Validator\Sexadecimal;
 use MarcoConsiglio\Goniometry\SexadecimalDegrees;
 use MarcoConsiglio\Goniometry\Tests\TestCase;
 use MarcoConsiglio\Goniometry\Traits\WithAngleFaker;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\TestDox;
+use PHPUnit\Framework\Attributes\UsesClass;
 use PHPUnit\Framework\Attributes\UsesTrait;
 
 #[TestDox("The SexadecimalDegrees class")]
 #[CoversClass(SexadecimalDegrees::class)]
 #[UsesTrait(WithAngleFaker::class)]
+#[UsesClass(NegativeSexadecimalGenerator::class)]
+#[UsesClass(NegativeSexadecimalValidator::class)]
+#[UsesClass(PositiveSexadecimalGenerator::class)]
+#[UsesClass(PositiveSexadecimalValidator::class)]
+#[UsesClass(RelativeSexadecimalGenerator::class)]
+#[UsesClass(RelativeSexadecimalValidator::class)]
+#[UsesClass(Sexadecimal::class)]
 class SexadecimalDegreesTest extends TestCase
 {
     #[TestDox("stores a sexadecimal value.")]
@@ -22,8 +37,8 @@ class SexadecimalDegreesTest extends TestCase
 
         // Act & Assert
         $this->assertEquals(
-            $this->safeRound($value),
-            $sexadecimal->value(self::PRECISION)
+            $value,
+            $sexadecimal->value()
         );
     }
 }
