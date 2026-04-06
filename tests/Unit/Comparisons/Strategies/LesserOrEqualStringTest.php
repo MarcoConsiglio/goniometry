d1ca5c67ad69a29fc4654c2b20ccfad6b5181912
Marco Consiglio <mrccnsgl@gmail.com>
2026-04-06 14:46:19 +0000

    refactor: `LesserOrEqualStringTest` class

--- tests/Unit/Comparisons/Strategies/LesserOrEqualStringTest.php
+++ tests/Unit/Comparisons/Strategies/LesserOrEqualStringTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace MarcoConsiglio\Goniometry\Tests\Unit\Comparisons\Strategies;
 
+use MarcoConsiglio\FakerPhpNumberHelpers\NextFloat;
 use MarcoConsiglio\Goniometry\Angle;
 use MarcoConsiglio\Goniometry\Builders\FromSexadecimal;
 use MarcoConsiglio\Goniometry\Builders\FromString;
@@ -13,6 +14,16 @@ use MarcoConsiglio\Goniometry\Comparisons\Strategies\LesserString;
 use MarcoConsiglio\Goniometry\Degrees;
 use MarcoConsiglio\Goniometry\Enums\Direction;
 use MarcoConsiglio\Goniometry\Minutes;
+use MarcoConsiglio\Goniometry\Random\Generator\Angle as AngleGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\NegativeAngle as NegativeAngleGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\NegativeSexadecimal as NegativeSexadecimalGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\PositiveAngle as PositiveAngleGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\PositiveSexadecimal as PositiveSexadecimalGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\RelativeAngle as RelativeAngleGenerator;
+use MarcoConsiglio\Goniometry\Random\Validator\NegativeSexadecimal as NegativeSexadecimalValidator;
+use MarcoConsiglio\Goniometry\Random\Validator\PositiveSexadecimal as PositiveSexadecimalValidator;
+use MarcoConsiglio\Goniometry\Random\Validator\RelativeSexadecimal as RelativeSexadecimalValidator;
+use MarcoConsiglio\Goniometry\Random\Validator\Sexadecimal as SexadecimalValidator;
 use MarcoConsiglio\Goniometry\Seconds;
 use MarcoConsiglio\Goniometry\SexadecimalDegrees;
 use MarcoConsiglio\Goniometry\SexagesimalDegrees;
@@ -20,24 +31,34 @@ use MarcoConsiglio\Goniometry\Tests\TestCase;
 use MarcoConsiglio\Goniometry\Traits\WithAngleFaker;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\TestDox;
-use PHPUnit\Framework\Attributes\UsesTrait;
 use PHPUnit\Framework\Attributes\UsesClass;
+use PHPUnit\Framework\Attributes\UsesTrait;
 
 #[TestDox("The LesserOrEqualString comparison strategy")]
 #[CoversClass(LesserOrEqualString::class)]
 #[UsesClass(Angle::class)]
-#[UsesClass(FromSexadecimal::class)]
-#[UsesClass(FromString::class)]
+#[UsesClass(AngleGenerator::class)]
+#[UsesClass(Degrees::class)]
+#[UsesClass(Direction::class)]
 #[UsesClass(EqualAngle::class)]
 #[UsesClass(EqualString::class)]
+#[UsesClass(FromSexadecimal::class)]
+#[UsesClass(FromString::class)]
 #[UsesClass(GreaterAngle::class)]
 #[UsesClass(LesserAngle::class)]
 #[UsesClass(LesserString::class)]
-#[UsesClass(Degrees::class)]
 #[UsesClass(Minutes::class)]
+#[UsesClass(NegativeAngleGenerator::class)]
+#[UsesClass(NegativeSexadecimalGenerator::class)]
+#[UsesClass(NegativeSexadecimalValidator::class)]
+#[UsesClass(PositiveAngleGenerator::class)]
+#[UsesClass(PositiveSexadecimalGenerator::class)]
+#[UsesClass(PositiveSexadecimalValidator::class)]
+#[UsesClass(RelativeAngleGenerator::class)]
+#[UsesClass(RelativeSexadecimalValidator::class)]
 #[UsesClass(Seconds::class)]
-#[UsesClass(Direction::class)]
 #[UsesClass(SexadecimalDegrees::class)]
+#[UsesClass(SexadecimalValidator::class)]
 #[UsesClass(SexagesimalDegrees::class)]
 #[UsesTrait(WithAngleFaker::class)]
 class LesserOrEqualStringTest extends TestCase
@@ -51,8 +72,8 @@ class LesserOrEqualStringTest extends TestCase
          * Lesser
          */
         // Arrange
-        $alfa = $this->randomAngle(max: 180 - self::SSN);
-        $beta = (string) $this->randomAngle(min: 180);
+        $alfa = $this->randomAngle(NextFloat::after(-180), NextFloat::before(180));
+        $beta = (string) $this->positiveRandomAngle(min: 180);
 
         // Act & Assert
         $this->assertTrue(new LesserOrEqualString($alfa, $beta)->compare());
@@ -71,8 +92,8 @@ class LesserOrEqualStringTest extends TestCase
          * Greater
          */
         // Arrange
-        $alfa = $this->randomAngle(min: 180);
-        $beta = (string) $this->randomAngle(max: 180 - self::SSN);
+        $alfa = $this->positiveRandomAngle(min: 180);
+        $beta = (string) $this->randomAngle(NextFloat::after(-180), NextFloat::before(180));
 
         // Act & Assert
         $this->assertFalse(new LesserOrEqualString($alfa, $beta)->compare());
