b329c05d764797da11d20d957004cf11dbb08019
Marco Consiglio <mrccnsgl@gmail.com>
2026-04-06 10:29:31 +0000

    refactor: `GreaterOrEqualTest` class

--- tests/Unit/Comparisons/GreaterOrEqualTest.php
+++ tests/Unit/Comparisons/GreaterOrEqualTest.php
@@ -21,6 +21,18 @@ use MarcoConsiglio\Goniometry\Comparisons\Types\StringType;
 use MarcoConsiglio\Goniometry\Degrees;
 use MarcoConsiglio\Goniometry\Enums\Direction;
 use MarcoConsiglio\Goniometry\Minutes;
+use MarcoConsiglio\Goniometry\Random\Generator\Angle as AngleGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\Degrees as DegreesGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\NegativeAngle as NegativeAngleGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\NegativeSexadecimal as NegativeSexadecimalGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\PositiveAngle as PositiveAngleGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\PositiveSexadecimal as PositiveSexadecimalGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\RelativeAngle as RelativeAngleGenerator;
+use MarcoConsiglio\Goniometry\Random\Validator\Degrees as DegreesValidator;
+use MarcoConsiglio\Goniometry\Random\Validator\NegativeSexadecimal as NegativeSexadecimalValidator;
+use MarcoConsiglio\Goniometry\Random\Validator\PositiveSexadecimal as PositiveSexadecimalValidator;
+use MarcoConsiglio\Goniometry\Random\Validator\RelativeSexadecimal as RelativeSexadecimalValidator;
+use MarcoConsiglio\Goniometry\Random\Validator\Sexadecimal as SexadecimalValidator;
 use MarcoConsiglio\Goniometry\Seconds;
 use MarcoConsiglio\Goniometry\SexadecimalDegrees;
 use MarcoConsiglio\Goniometry\SexagesimalDegrees;
@@ -35,27 +47,40 @@ use PHPUnit\Framework\Attributes\UsesClass;
 #[TestDox("The GreaterOrEqual comparison")]
 #[CoversClass(GreaterOrEqual::class)]
 #[UsesClass(Angle::class)]
+#[UsesClass(AngleGenerator::class)]
+#[UsesClass(AngleType::class)]
+#[UsesClass(ComparisonStrategy::class)]
+#[UsesClass(Degrees::class)]
+#[UsesClass(DegreesGenerator::class)]
+#[UsesClass(DegreesValidator::class)]
+#[UsesClass(Direction::class)]
+#[UsesClass(EqualAngle::class)]
+#[UsesClass(FloatComparisonStrategy::class)]
+#[UsesClass(FloatType::class)]
 #[UsesClass(FromSexadecimal::class)]
 #[UsesClass(FromSexagesimal::class)]
 #[UsesClass(FromString::class)]
-#[UsesClass(ComparisonStrategy::class)]
-#[UsesClass(FloatComparisonStrategy::class)]
-#[UsesClass(EqualAngle::class)]
 #[UsesClass(GreaterAngle::class)]
 #[UsesClass(GreaterOrEqualAngle::class)]
-#[UsesClass(GreaterOrEqualInt::class)]
 #[UsesClass(GreaterOrEqualFloat::class)]
+#[UsesClass(GreaterOrEqualInt::class)]
 #[UsesClass(GreaterOrEqualString::class)]
-#[UsesClass(AngleType::class)]
 #[UsesClass(IntType::class)]
-#[UsesClass(FloatType::class)]
-#[UsesClass(StringType::class)]
-#[UsesClass(Degrees::class)]
 #[UsesClass(Minutes::class)]
+#[UsesClass(NegativeAngleGenerator::class)]
+#[UsesClass(NegativeSexadecimalGenerator::class)]
+#[UsesClass(NegativeSexadecimalValidator::class)]
+#[UsesClass(PositiveAngleGenerator::class)]
+#[UsesClass(PositiveSexadecimalGenerator::class)]
+#[UsesClass(PositiveSexadecimalValidator::class)]
+#[UsesClass(RelativeAngleGenerator::class)]
+#[UsesClass(RelativeSexadecimalValidator::class)]
+#[UsesClass(RelativeSexadecimalValidator::class)]
 #[UsesClass(Seconds::class)]
-#[UsesClass(Direction::class)]
 #[UsesClass(SexadecimalDegrees::class)]
+#[UsesClass(SexadecimalValidator::class)]
 #[UsesClass(SexagesimalDegrees::class)]
+#[UsesClass(StringType::class)]
 #[UsesTrait(WithAngleFaker::class)]
 class GreaterOrEqualTest extends TestCase
 {
@@ -83,7 +108,7 @@ class GreaterOrEqualTest extends TestCase
     public function test_compare_int(): void
     {
         // Act & Assert
-        $comparison = new GreaterOrEqual($this->alfa, $this->randomDegrees());
+        $comparison = new GreaterOrEqual($this->alfa, $this->randomDegrees()->value());
         $this->assertIsBool($comparison->compare());
     }
 
@@ -93,7 +118,7 @@ class GreaterOrEqualTest extends TestCase
         // Act & Assert
         $comparison = new GreaterOrEqual(
             $this->alfa, 
-            $this->positiveRandomFloat()
+            $this->randomFloat()
         );
         $comparison->setPrecision($this->randomPrecision());
         $this->assertIsBool($comparison->compare());
