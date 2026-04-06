271612bb0fac0d190af433014340f84cb2f9bc5c
Marco Consiglio <mrccnsgl@gmail.com>
2026-04-06 10:54:07 +0000

    refactor: `DifferentAngleTest` class

--- tests/Unit/Comparisons/Strategies/DifferentAngleTest.php
+++ tests/Unit/Comparisons/Strategies/DifferentAngleTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace MarcoConsiglio\Goniometry\Tests\Unit\Comparisons\Strategies;
 
+use MarcoConsiglio\FakerPhpNumberHelpers\NextFloat;
 use MarcoConsiglio\Goniometry\Angle;
 use MarcoConsiglio\Goniometry\Builders\FromSexadecimal;
 use MarcoConsiglio\Goniometry\Comparisons\Strategies\DifferentAngle;
@@ -8,6 +9,16 @@ use MarcoConsiglio\Goniometry\Comparisons\Strategies\EqualAngle;
 use MarcoConsiglio\Goniometry\Degrees;
 use MarcoConsiglio\Goniometry\Enums\Direction;
 use MarcoConsiglio\Goniometry\Minutes;
+use MarcoConsiglio\Goniometry\Random\Generator\Angle as AngleGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\NegativeAngle as NegativeAngleGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\NegativeSexadecimal as NegativeSexadecimalGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\PositiveAngle as PositiveAngleGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\PositiveSexadecimal as PositiveSexadecimalGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\RelativeAngle as RelativeAngleGenerator;
+use MarcoConsiglio\Goniometry\Random\Validator\NegativeSexadecimal as ValidatorNegativeSexadecimal;
+use MarcoConsiglio\Goniometry\Random\Validator\PositiveSexadecimal as ValidatorPositiveSexadecimal;
+use MarcoConsiglio\Goniometry\Random\Validator\RelativeSexadecimal as RelativeSexadecimalValidator;
+use MarcoConsiglio\Goniometry\Random\Validator\Sexadecimal as SexadecimalValidator;
 use MarcoConsiglio\Goniometry\Seconds;
 use MarcoConsiglio\Goniometry\SexadecimalDegrees;
 use MarcoConsiglio\Goniometry\SexagesimalDegrees;
@@ -21,14 +32,24 @@ use PHPUnit\Framework\Attributes\UsesClass;
 #[TestDox("The DifferentAngle comparison strategy")]
 #[CoversClass(DifferentAngle::class)]
 #[UsesClass(Angle::class)]
-#[UsesClass(FromSexadecimal::class)]
-#[UsesClass(EqualAngle::class)]
+#[UsesClass(AngleGenerator::class)]
 #[UsesClass(Degrees::class)]
-#[UsesClass(Minutes::class)]
-#[UsesClass(Seconds::class)]
 #[UsesClass(Direction::class)]
+#[UsesClass(EqualAngle::class)]
+#[UsesClass(FromSexadecimal::class)]
+#[UsesClass(Minutes::class)]
+#[UsesClass(NegativeAngleGenerator::class)]
+#[UsesClass(NegativeSexadecimalGenerator::class)]
+#[UsesClass(PositiveAngleGenerator::class)]
+#[UsesClass(PositiveSexadecimalGenerator::class)]
+#[UsesClass(RelativeAngleGenerator::class)]
+#[UsesClass(RelativeSexadecimalValidator::class)]
+#[UsesClass(Seconds::class)]
 #[UsesClass(SexadecimalDegrees::class)]
+#[UsesClass(SexadecimalValidator::class)]
 #[UsesClass(SexagesimalDegrees::class)]
+#[UsesClass(ValidatorNegativeSexadecimal::class)]
+#[UsesClass(ValidatorPositiveSexadecimal::class)]
 #[UsesTrait(WithAngleFaker::class)]
 class DifferentAngleTest extends TestCase
 {
@@ -41,7 +62,7 @@ class DifferentAngleTest extends TestCase
          * Different
          */
         // Arrange
-        $alfa = $this->randomAngle(max: 180 - self::SSN);
+        $alfa = $this->randomAngle(max: NextFloat::before(180));
         $beta = $this->randomAngle(min: 180);
 
         // Act & Assert
