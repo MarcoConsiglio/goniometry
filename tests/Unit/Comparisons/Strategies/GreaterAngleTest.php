a5a6b1f54a8c8d47f5c6e7631d5e4973daf829dd
Marco Consiglio <mrccnsgl@gmail.com>
2026-04-06 12:49:47 +0000

    refactor: `GreaterAngleTest` class

--- tests/Unit/Comparisons/Strategies/GreaterAngleTest.php
+++ tests/Unit/Comparisons/Strategies/GreaterAngleTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace MarcoConsiglio\Goniometry\Tests\Unit\Comparisons\Strategies;
 
+use MarcoConsiglio\FakerPhpNumberHelpers\NextFloat;
 use MarcoConsiglio\Goniometry\Angle;
 use MarcoConsiglio\Goniometry\Builders\FromSexadecimal;
 use MarcoConsiglio\Goniometry\Builders\FromSexagesimal;
@@ -8,6 +9,22 @@ use MarcoConsiglio\Goniometry\Comparisons\Strategies\GreaterAngle;
 use MarcoConsiglio\Goniometry\Degrees;
 use MarcoConsiglio\Goniometry\Enums\Direction;
 use MarcoConsiglio\Goniometry\Minutes;
+use MarcoConsiglio\Goniometry\Random\Generator\Angle as AngleGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\Degrees as DegreesGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\Minutes as MinutesGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\NegativeAngle as NegativeAngleGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\NegativeSexadecimal as NegativeSexadecimalGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\PositiveAngle as PositiveAngleGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\PositiveSexadecimal as PositiveSexadecimalGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\RelativeAngle as RelativeAngleGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\Seconds as SecondsGenerator;
+use MarcoConsiglio\Goniometry\Random\Validator\Degrees as DegreesValidator;
+use MarcoConsiglio\Goniometry\Random\Validator\Minutes as MinutesValidator;
+use MarcoConsiglio\Goniometry\Random\Validator\NegativeSexadecimal as NegativeSexadecimalValidator;
+use MarcoConsiglio\Goniometry\Random\Validator\PositiveSexadecimal as PositiveSexadecimalValidator;
+use MarcoConsiglio\Goniometry\Random\Validator\RelativeSexadecimal as RelativeSexadecimalValidator;
+use MarcoConsiglio\Goniometry\Random\Validator\Seconds as SecondsValidator;
+use MarcoConsiglio\Goniometry\Random\Validator\Sexadecimal as SexadecimalValidator;
 use MarcoConsiglio\Goniometry\Seconds;
 use MarcoConsiglio\Goniometry\SexadecimalDegrees;
 use MarcoConsiglio\Goniometry\SexagesimalDegrees;
@@ -21,13 +38,29 @@ use PHPUnit\Framework\Attributes\UsesClass;
 #[TestDox("The GreateAngle comparison strategy")]
 #[CoversClass(GreaterAngle::class)]
 #[UsesClass(Angle::class)]
+#[UsesClass(AngleGenerator::class)]
+#[UsesClass(Degrees::class)]
+#[UsesClass(DegreesGenerator::class)]
+#[UsesClass(DegreesValidator::class)]
+#[UsesClass(Direction::class)]
 #[UsesClass(FromSexadecimal::class)]
 #[UsesClass(FromSexagesimal::class)]
-#[UsesClass(Degrees::class)]
 #[UsesClass(Minutes::class)]
+#[UsesClass(MinutesGenerator::class)]
+#[UsesClass(MinutesValidator::class)]
+#[UsesClass(NegativeAngleGenerator::class)]
+#[UsesClass(NegativeSexadecimalGenerator::class)]
+#[UsesClass(NegativeSexadecimalValidator::class)]
+#[UsesClass(PositiveAngleGenerator::class)]
+#[UsesClass(PositiveSexadecimalGenerator::class)]
+#[UsesClass(PositiveSexadecimalValidator::class)]
+#[UsesClass(RelativeAngleGenerator::class)]
+#[UsesClass(RelativeSexadecimalValidator::class)]
 #[UsesClass(Seconds::class)]
-#[UsesClass(Direction::class)]
+#[UsesClass(SecondsGenerator::class)]
+#[UsesClass(SecondsValidator::class)]
 #[UsesClass(SexadecimalDegrees::class)]
+#[UsesClass(SexadecimalValidator::class)]
 #[UsesClass(SexagesimalDegrees::class)]
 #[UsesTrait(WithAngleFaker::class)]
 class GreaterAngleTest extends TestCase
@@ -42,22 +75,22 @@ class GreaterAngleTest extends TestCase
          */
         // Arrange
         $alfa = Angle::createFromValues(
-            degrees: $this->randomDegrees(min: 180)
+            degrees: $this->randomDegrees(min: 180)->value()
         );
         $beta = Angle::createFromValues(
-            degrees: $this->randomDegrees(max: 179)
+            degrees: $this->randomDegrees(max: 179)->value()
         );
         $gamma = Angle::createFromValues(
-            minutes: $this->randomMinutes(min: 30)
+            minutes: $this->randomMinutes(min: 30)->value()
         );
         $delta = Angle::createFromValues(
-            minutes: $this->randomMinutes(max: 29)
+            minutes: $this->randomMinutes(max: 29)->value()
         );
         $epsilon = Angle::createFromValues(
-            seconds: $this->randomSeconds(min: 30.0)
+            seconds: $this->randomSeconds(min: 30.0)->value()
         );
         $zeta = Angle::createFromValues(
-            seconds: $this->randomSeconds(max: 30.0 - self::SSN)
+            seconds: $this->randomSeconds(max: NextFloat::before(30.0))->value()
         );
 
         // Act & Assert
