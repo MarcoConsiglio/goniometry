00ac70d7ed90e759d6767359807df74f2b002414
Marco Consiglio <mrccnsgl@gmail.com>
2026-04-06 14:30:12 +0000

    refactor: `LesserFloatTest` class

--- tests/Unit/Comparisons/Strategies/LesserFloatTest.php
+++ tests/Unit/Comparisons/Strategies/LesserFloatTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace MarcoConsiglio\Goniometry\Tests\Unit\Comparisons\Strategies;
 
+use MarcoConsiglio\FakerPhpNumberHelpers\NextFloat;
 use MarcoConsiglio\Goniometry\Angle;
 use MarcoConsiglio\Goniometry\Builders\FromSexadecimal;
 use MarcoConsiglio\Goniometry\Casting\Sexadecimal\Round;
@@ -8,6 +9,17 @@ use MarcoConsiglio\Goniometry\Comparisons\Strategies\LesserFloat;
 use MarcoConsiglio\Goniometry\Degrees;
 use MarcoConsiglio\Goniometry\Enums\Direction;
 use MarcoConsiglio\Goniometry\Minutes;
+use MarcoConsiglio\Goniometry\Random\Generator\Angle as AngleGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\NegativeAngle as NegativeAngleGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\NegativeSexadecimal as NegativeSexadecimalGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\PositiveAngle as PositiveAngleGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\PositiveSexadecimal as PositiveSexadecimalGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\RelativeAngle as RelativeAngleGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\RelativeSexadecimal as RelativeSexadecimalGenerator;
+use MarcoConsiglio\Goniometry\Random\Validator\NegativeSexadecimal as NegativeSexadecimalValidator;
+use MarcoConsiglio\Goniometry\Random\Validator\PositiveSexadecimal as PositiveSexadecimalValidator;
+use MarcoConsiglio\Goniometry\Random\Validator\RelativeSexadecimal as RelativeSexadecimalValidator;
+use MarcoConsiglio\Goniometry\Random\Validator\Sexadecimal as SexadecimalValidator;
 use MarcoConsiglio\Goniometry\Seconds;
 use MarcoConsiglio\Goniometry\SexadecimalDegrees;
 use MarcoConsiglio\Goniometry\SexagesimalDegrees;
@@ -15,19 +27,30 @@ use MarcoConsiglio\Goniometry\Tests\TestCase;
 use MarcoConsiglio\Goniometry\Traits\WithAngleFaker;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\TestDox;
-use PHPUnit\Framework\Attributes\UsesTrait;
 use PHPUnit\Framework\Attributes\UsesClass;
+use PHPUnit\Framework\Attributes\UsesTrait;
 
 #[TestDox("The LesserFloat comparison strategy")]
 #[CoversClass(LesserFloat::class)]
 #[UsesClass(Angle::class)]
-#[UsesClass(FromSexadecimal::class)]
+#[UsesClass(AngleGenerator::class)]
 #[UsesClass(Degrees::class)]
-#[UsesClass(Minutes::class)]
-#[UsesClass(Seconds::class)]
 #[UsesClass(Direction::class)]
+#[UsesClass(FromSexadecimal::class)]
+#[UsesClass(Minutes::class)]
+#[UsesClass(NegativeAngleGenerator::class)]
+#[UsesClass(NegativeSexadecimalGenerator::class)]
+#[UsesClass(NegativeSexadecimalValidator::class)]
+#[UsesClass(PositiveAngleGenerator::class)]
+#[UsesClass(PositiveSexadecimalGenerator::class)]
+#[UsesClass(PositiveSexadecimalValidator::class)]
+#[UsesClass(RelativeAngleGenerator::class)]
+#[UsesClass(RelativeSexadecimalGenerator::class)]
+#[UsesClass(RelativeSexadecimalValidator::class)]
 #[UsesClass(Round::class)]
+#[UsesClass(Seconds::class)]
 #[UsesClass(SexadecimalDegrees::class)]
+#[UsesClass(SexadecimalValidator::class)]
 #[UsesClass(SexagesimalDegrees::class)]
 #[UsesTrait(WithAngleFaker::class)]
 class LesserFloatTest extends TestCase
@@ -41,8 +64,8 @@ class LesserFloatTest extends TestCase
          * Lesser
          */
         // Arrange
-        $alfa = $this->randomAngle(max: 180 - self::SSN);
-        $beta = $this->randomSexadecimal(min: 180);
+        $alfa = $this->randomAngle(NextFloat::after(-180), NextFloat::before(180));
+        $beta = $this->positiveRandomSexadecimal(min: 180);
 
         // Act & Assert
         $this->assertTrue(
@@ -54,7 +77,7 @@ class LesserFloatTest extends TestCase
          * Equal
          */
         // Arrange
-        $alfa = $this->randomAngle();
+        $alfa = $this->randomAngle(precision: 1);
         $beta = $alfa->toFloat();
 
         // Act & Assert
@@ -67,8 +90,8 @@ class LesserFloatTest extends TestCase
          * Greater
          */
         // Arrange
-        $alfa = $this->randomAngle(min: 180);
-        $beta = $this->randomSexadecimal(max: 180 - self::SSN);
+        $alfa = $this->positiveRandomAngle(min: 180);
+        $beta = $this->randomSexadecimal(NextFloat::after(-180), NextFloat::before(180));
 
         // Act & Assert
         $this->assertFalse(
