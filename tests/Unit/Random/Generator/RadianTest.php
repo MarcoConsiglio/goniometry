c7639520cc2b730d0b2c8bc5db87962f049f4f95
Marco Consiglio <mrccnsgl@gmail.com>
2026-04-07 14:03:30 +0000

    refactor: `RadianTest` to have a better test coverage

---
+++ tests/Unit/Random/Generator/RadianTest.php
@@ -0,0 +1,60 @@
+<?php
+namespace MarcoConsiglio\Goniometry\Tests\Unit\Random\Generator;
+
+use MarcoConsiglio\Goniometry\Radian;
+use MarcoConsiglio\Goniometry\Random\Generator\PositiveRadian as PositiveRadianGenerator;
+use MarcoConsiglio\Goniometry\Random\Generator\Radian as RadianGenerator;
+use MarcoConsiglio\Goniometry\Random\RadianRange;
+use MarcoConsiglio\Goniometry\Random\Validator\PositiveRadian as PositiveRadianValidator;
+use MarcoConsiglio\Goniometry\Random\Validator\Radian as RadianValidator;
+use MarcoConsiglio\Goniometry\Tests\TestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
+
+#[CoversClass(RadianGenerator::class)]
+#[UsesClass(Radian::class)]
+#[UsesClass(PositiveRadianGenerator::class)]
+#[UsesClass(RadianRange::class)]
+#[UsesClass(PositiveRadianValidator::class)]
+#[UsesClass(RadianValidator::class)]
+class RadianTest extends TestCase
+{
+    public function test_normalize_precision(): void
+    {
+        /**
+         * Precision higher than PHP_FLOAT_DIG
+         */
+        // Arrange
+        $generator = new PositiveRadianGenerator(
+            self::$faker,
+            new PositiveRadianValidator,
+            new RadianRange(RadianRange::min(), RadianRange::max())
+        );
+
+        // Act
+        $radian = $generator->generate(
+            $this->positiveRandomInteger(min: PHP_FLOAT_DIG + 1)
+        );
+
+        // Assert
+        $this->assertEquals(PHP_FLOAT_DIG, $radian->value->scale);
+
+        /**
+         * Precision lower or equal to PHP_FLOAT_DIG
+         */
+        // Arrange
+        $generator = new PositiveRadianGenerator(
+            self::$faker,
+            new PositiveRadianValidator,
+            new RadianRange(RadianRange::min(), RadianRange::max())
+        );
+
+        // Act
+        $radian = $generator->generate(
+            $precision = $this->positiveRandomInteger(max: PHP_FLOAT_DIG)
+        );
+
+        // Assert
+        $this->assertEquals($precision, $radian->value->scale);
+    }
+}
