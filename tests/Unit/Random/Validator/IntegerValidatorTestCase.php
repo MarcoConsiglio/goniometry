9e80f6b3ca5993722439b9f5403deb1242a9f9d0
Marco Consiglio <mrccnsgl@gmail.com>
2026-04-09 16:10:02 +0000

    fix: `Validator\MinutesTest` class

--- tests/Unit/Random/Validator/IntegerValidatorTestCase.php
+++ tests/Unit/Random/Validator/IntegerValidatorTestCase.php
@@ -18,14 +18,14 @@ abstract class IntegerValidatorTestCase extends TestCase
 
     abstract protected function allowedMax(): int;
 
-    protected function testValidator(): void
+    protected function testValidator(int $expected_min, int $expected_max): void
     {
         // Act
         $this->validator->validate($this->min, $this->max);
 
         // Assert
-        $this->assertSame($this->allowedMin(), $this->min);
-        $this->assertSame($this->allowedMax(), $this->max);
+        $this->assertSame($expected_min, $this->min);
+        $this->assertSame($expected_max, $this->max);
     }
 
     protected function setRange(int $min, int $max)
